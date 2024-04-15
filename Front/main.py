0bfacc0a0f435318c23d3fe0ee3302ecd76403e6
Czechu7 <choocas0@gmail.com>
2024-04-15 13:10:36 +0200

    Front structure

---
+++ Front/main.py
@@ -0,0 +1,9 @@
+import imports
+
+def main():
+    print("Uruchomiono główny moduł.")
+    #Poniżej nalezy inicjowac moduly
+    imports.helloworld.hello_world()
+
+if __name__ == "__main__":
+    main()
