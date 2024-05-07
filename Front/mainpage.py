ee306fc750bb1720956df0fcdd28deddd903d8f6
Kyrylo <stupackijk@gmail.com>
2024-05-07 19:05:04 +0200

    update mainpage

--- Front/mainpage.py
+++ Front/mainpage.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QMainWindow, QAction, QLabel, QPushButton, QMessageBox, QVBoxLayout, QWidget
+from PyQt5.QtWidgets import QMainWindow, QAction, QLabel, QPushButton, QMessageBox, QVBoxLayout, QFrame
 from PyQt5.QtCore import Qt
 import imports as imports
 
@@ -7,7 +7,7 @@ class MainMenu(QMainWindow):
         super().__init__()
 
         self.setWindowTitle("Menu Główne")
-        self.setGeometry(100, 100, 900, 900)
+        self.setGeometry(100, 100, 800, 600)
 
         self.initUI()
 
@@ -20,24 +20,57 @@ class MainMenu(QMainWindow):
         menubar = self.menuBar()
         file_menu = menubar.addMenu("Plik")
         file_menu.addAction(action_exit)
-        
-        # Tworzymy powitanie
-        self.welcome_label = QLabel("Witaj! Zanim zaczniemy, Zaloguj się!.", self)
-        self.welcome_label.setAlignment(Qt.AlignCenter)
 
-        # Ustawiamy layout dla okna
-        layout = QVBoxLayout()
-        layout.addWidget(self.welcome_label)
+        # Ustawiamy tło
+        self.setStyleSheet("MainMenu { background-image: url(bg.png); background-position: center; background-repeat: no-repeat; background-attachment: fixed;}")
 
-        # Tworzymy widget i ustawiamy layout
-        central_widget = QWidget(self)
-        central_widget.setLayout(layout)
-        self.setCentralWidget(central_widget)
+        # Tworzymy overlay
+        self.overlay = QFrame(self)
+        self.overlay.setStyleSheet("background-color: rgba(45, 54, 81, 0.8);")
+        self.overlay.setGeometry(0, menubar.height(), self.width(), self.height() - menubar.height())
 
-        self.login_button = QPushButton("Zaloguj się")  # Przypisanie do atrybutu
-        self.register_button = QPushButton("Zarejestruj się")  # Przypisanie do atrybutu
+        layout = QVBoxLayout(self.overlay)
+
+        # Text
+        self.label = QLabel("Witaj! Zanim zaczniemy, Zaloguj się!", self)
+        self.label.setWordWrap(True)
+        self.label.setStyleSheet("color: white; font-size: 24px; background-color: none ") 
+        self.label.setAlignment(Qt.AlignCenter)
+
+        # Tworzymy przyciski oraz style do nich
+        self.login_button = QPushButton("Zaloguj się") 
+        self.login_button.setStyleSheet('''
+            QPushButton {
+                color: white;
+                font-size: 16px;
+                padding: 0.5em 1em;
+                border: 2px solid white;
+                border-radius: 5px;
+            }
+            QPushButton:hover {
+                background-color: rgba(255, 255, 255, 0.2);
+            }
+        ''')
+
+        self.register_button = QPushButton("Zarejestruj się")  
+        self.register_button.setStyleSheet('''
+           QPushButton {
+                color: white;
+                font-size: 16px;
+                padding: 0.5em 1em;
+                border: 2px solid white;
+                border-radius: 5px;
+            }
+            QPushButton:hover {
+                background-color: rgba(255, 255, 255, 0.2);
+            }
+        ''')
+
+        # Actions do przycisków
         self.login_button.clicked.connect(self.openLogin)
         self.register_button.clicked.connect(self.openRegister)
+
+        layout.addWidget(self.label)
         layout.addWidget(self.login_button)
         layout.addWidget(self.register_button)
 
