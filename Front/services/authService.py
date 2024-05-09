d97808dff71f061f64f49560fc35d7317fd3bf98
kkolcz <vipciorr@gmail.com>
2024-05-09 15:12:29 +0200

    remove unnecessary boilerplate code and code descript

--- Front/services/authService.py
+++ Front/services/authService.py
@@ -5,8 +5,9 @@ import os
 import time
 
 API_URL = 'http://localhost:8080/api/v1'
+TOKEN_FILE = 'token.txt'
 
-# Rejestracja nowego konta
+# REJESTRACJA NOWEGO KONTA
 def signUp(name, password):
     body = {'name': name, 'password': password}
     res = requests.post(API_URL + '/sign-up', json=body)
@@ -14,72 +15,84 @@ def signUp(name, password):
         resJson = res.json()
         tokenDecrypt = jwt.decode(resJson['token'], algorithms="HS256", options={"verify_signature": False})
         user = User(resJson['user']['id'], resJson['user']['name'], resJson['user']['role'], resJson['token'], tokenDecrypt['iat'], tokenDecrypt['exp'])
-        with open('token.txt', 'w') as plik:
+        with open(TOKEN_FILE, 'w') as plik:
             plik.write(user.token)
         return user; #Obiekt użytkownika User
     else:
         raise Exception('Błąd rejestracji!')
 
 
-# Logowanie
+# LOGOWANIE
 def signIn(name, password):
     body = {'name': name, 'password': password}
     res = requests.post(API_URL + '/sign-in', json=body)
     if(res.status_code == 200): # 200 Successful
         resJson = res.json()
-        tokenDecrypt = jwt.decode(resJson['token'], algorithms="HS256", options={"verify_signature": False})
+        token = resJson['token']
+        tokenDecrypt = decodeToken(token)
         user = User(resJson['user']['id'], resJson['user']['name'], resJson['user']['role'], resJson['token'], tokenDecrypt['iat'], tokenDecrypt['exp'])     
-        with open('token.txt', 'w') as plik:
+        with open(TOKEN_FILE, 'w') as plik:
             plik.write(user.token)
         return user; #Obiekt użytkownika User
     else:
         raise Exception('Błąd logowania!')
-
+    
+# WERYFIKACJA CZY UZYTKOWNIK JEST ZALOGOWANY
 def isUserLogged():
-    if os.path.exists('token.txt'):
-        with open('token.txt', 'r') as plik:
-            token = plik.read().strip()
-        try:
-            tokenDecrypt = jwt.decode(token, algorithms="HS256", options={"verify_signature": False})
-            if tokenDecrypt['exp'] > time.time():
-                return True
-            else:
-                print('Token wygasł.')
-                return False
-        except jwt.InvalidTokenError:
-            print('Nieprawidłowy token.')
-            return False
-    else:
+    token = loadFileToken()
+    if(token == None): 
         print('Token nie istnieje.')
+        return None
+    try:
+        tokenDecrypt = decodeToken(token)
+        if tokenDecrypt['exp'] > time.time():
+            return True
+        else:
+            print('Token wygasł.')
+            return False
+    except jwt.InvalidTokenError:
+        print('Nieprawidłowy token.')
         return False
     
+# WYLOGOWANIE
 def logout():
-    if os.path.exists('token.txt'):
-        os.remove('token.txt')
+    if os.path.exists(TOKEN_FILE):
+        os.remove(TOKEN_FILE)
         print('Wylogowano pomyślnie.')
     else:
         print('Brak zalogowanego użytkownika.')
 
+# POBRANIE INFORMACJI O UŻYTKOWNIKU
 def getLoggedUserInfo():
-    if os.path.exists('token.txt'):
-        with open('token.txt', 'r') as plik:
-            token = plik.read().strip()
-        try:
-            tokenDecrypt = jwt.decode(token, algorithms="HS256", options={"verify_signature": False})
-            user_info = {
-                'role': tokenDecrypt.get('scopes'),
-                'user_name': tokenDecrypt.get('sub'),
-                'iat': tokenDecrypt.get('iat'),
-                'token': token,
-                'expiration_time': tokenDecrypt.get('exp')
-            }
-            return user_info
-        except jwt.InvalidTokenError:
-            print('Nieprawidłowy token.')
-            return None
-    else:
+    token = loadFileToken()
+    if(token == None): 
         print('Token nie istnieje.')
         return None
+    try:
+        tokenDecrypt = decodeToken(token)
+        user_info = {
+            'role': tokenDecrypt.get('scopes'),
+            'user_name': tokenDecrypt.get('sub'),
+            'iat': tokenDecrypt.get('iat'),
+            'token': token,
+            'expiration_time': tokenDecrypt.get('exp')
+        }
+        return user_info
+    except jwt.InvalidTokenError:
+        print('Nieprawidłowy token.')
+        return None
+        
 
+# FUNKCJE POMOCNICZE
+# WCZYTANIE TOKENU Z PLIKU
+def loadFileToken():
+    if os.path.exists(TOKEN_FILE):
+        with open(TOKEN_FILE, 'r') as plik:
+            token = plik.read().strip()
+        return token
+    else:
+        return None
 
-
+# DEKODOWANIE TOKENU
+def decodeToken(token):
+    return jwt.decode(token, algorithms="HS256", options={"verify_signature": False})
